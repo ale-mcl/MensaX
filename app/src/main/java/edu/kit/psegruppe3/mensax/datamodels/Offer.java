218fc0df1abc1a5d1f322f045f351fb288c31aa5
stefanomcl <stefano.miceli@web.de>
2015-06-19 21:05:53 +0200

    Added methods to Datenmodel-Klassen.

--- app/src/main/java/edu/kit/psegruppe3/mensax/datamodels/Offer.java
+++ app/src/main/java/edu/kit/psegruppe3/mensax/datamodels/Offer.java
@@ -1,9 +1,19 @@
 package edu.kit.psegruppe3.mensax.datamodels;
 
-/**
- * Created by ekremsenturk on 19.06.15.
- */
 public class Offer {
     private Meal meal;
     private int price;
+
+    public Offer(Meal meal, int price) {
+        this.meal = meal;
+        this. price = price;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+    public Meal getMeal() {
+        return meal;
+    }
 }
--- app/src/main/java/edu/kit/psegruppe3/mensax/datamodels/Tag.java
+++ app/src/main/java/edu/kit/psegruppe3/mensax/datamodels/Tag.java
@@ -1,10 +1,11 @@
 package edu.kit.psegruppe3.mensax.datamodels;
 
-/**
- * Created by ekremsenturk on 19.06.15.
- */
 public enum Tag {
+    BEEF,
+    BEEF_WELFARE,
+    PORK,
     VEGETARIAN,
     VEGAN,
-    PORK,
+    FISCH,
+    BIO
 }
