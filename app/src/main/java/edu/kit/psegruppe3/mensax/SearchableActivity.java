d91efc73180062ba65f2cdf3e93a4338f5865bed
stefanomcl <stefano.miceli@web.de>
2015-06-20 13:25:57 +0200

    Added Search function.

---
+++ app/src/main/java/edu/kit/psegruppe3/mensax/SearchableActivity.java
@@ -0,0 +1,56 @@
+package edu.kit.psegruppe3.mensax;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+/**
+ * Activity that performs searches and present results.
+ */
+public class SearchableActivity extends ListActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchable);
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doMySearch(query);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the options menu from XML
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+
+        return true;
+    }
+
+    public void doMySearch(String query) {
+        //method where to implement the search
+        // use full-text search FTS3 because we have a SQLite database!
+        //"If your data comes from a SQLite database query, you can apply your
+        // results to a ListView using a CursorAdapter."
+    }
+
+
+
+}
