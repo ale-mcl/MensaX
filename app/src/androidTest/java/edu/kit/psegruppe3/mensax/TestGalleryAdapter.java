48c8a9a70bbb5361d20e7fd74297c4e222fc2e07
Ekrem Sentürk <emin.sentuerk@googlemail.com>
2015-08-23 18:03:01 +0200

    Test bugfix

--- app/src/androidTest/java/edu/kit/psegruppe3/mensax/TestGalleryAdapter.java
+++ app/src/androidTest/java/edu/kit/psegruppe3/mensax/TestGalleryAdapter.java
@@ -2,9 +2,7 @@ package edu.kit.psegruppe3.mensax;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.test.AndroidTestCase;
-import android.view.View;
 
 /**
  * Tests the functionality of the {@link GalleryAdapter} class.
@@ -14,37 +12,22 @@ import android.view.View;
  */
 public class TestGalleryAdapter extends AndroidTestCase {
 
-    private GalleryAdapter mGalleryAdapter;
-
     /**
      * This test checks to make sure that all methods of the GalleryAdapter class work correctly.
      */
-    protected void testGalleryAdapter() {
-        mGalleryAdapter = new GalleryAdapter(getContext());
+    public void testGalleryAdapter() {
+        GalleryAdapter galleryAdapter = new GalleryAdapter(getContext());
         int imageCount = 5;
         Bitmap bitmap = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.no_image_available);
         for (int i = 0; i < imageCount ; i++) {
-            mGalleryAdapter.addBitmap(bitmap);
+            galleryAdapter.addBitmap(bitmap);
         }
-        assertEquals("Error: Value '" + mGalleryAdapter.getCount() +
+        assertEquals("Error: Value '" + galleryAdapter.getCount() +
                 "' did not match the expected value '" +
-                imageCount + "'.", imageCount, mGalleryAdapter.getCount());
-        assertEquals("Error: Returned object did not match the inserted image", bitmap, mGalleryAdapter.getItem(0));
-        assertEquals("Error: Value '" + mGalleryAdapter.getItemId(0) +
+                imageCount + "'.", imageCount, galleryAdapter.getCount());
+        assertEquals("Error: Returned object did not match the inserted image", bitmap, galleryAdapter.getItem(0));
+        assertEquals("Error: Value '" + galleryAdapter.getItemId(0) +
                 "' did not match the expected value '" +
-                0 + "'.", mGalleryAdapter.getItemId(0), 0);
-        View view = mGalleryAdapter.getView(0, null, null);
-        assertNotNull("Error: Returned view has no image", loadBitmapFromView(view));
-        assertEquals("Error: Image of the returned view did not match the expected image", loadBitmapFromView(view), bitmap);
-
-
-    }
-
-    private Bitmap loadBitmapFromView(View v) {
-        Bitmap b = Bitmap.createBitmap( v.getLayoutParams().width, v.getLayoutParams().height, Bitmap.Config.ARGB_8888);
-        Canvas c = new Canvas(b);
-        v.layout(0, 0, v.getLayoutParams().width, v.getLayoutParams().height);
-        v.draw(c);
-        return b;
+                0 + "'.", galleryAdapter.getItemId(0), 0);
     }
 }
